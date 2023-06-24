fa644b3a029f2979ad4bc15a400b018a300ccbe5
VlasiukOleg <ikslprint@gmail.com>
2023-06-24 20:17:56 +0300

    corrected mistakes

--- src/redux/phoneBookSlice.js
+++ src/redux/phoneBookSlice.js
@@ -28,11 +28,9 @@ const phoneBookSlice = createSlice({
       },
     },
     deleteContact: (state, action) => {
-      //   return state.contacts.filter(contact => contact.id !== action.payload);
-      const index = state.contacts.findIndex(
-        contact => contact.id === action.payload
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
       );
-      state.contacts.splice(index, 1);
     },
     changeFilter: (state, action) => {
       state.filter = action.payload;
